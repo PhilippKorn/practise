9f91886db122ce0a76b64e854506a5489dd2fd27
Philipp Korn <philipp.korn@ges.thm.de>
2018-10-08 10:48:36 +0200

    added background and ellipse

--- sketch.js
+++ sketch.js
@@ -1,7 +1,12 @@
+let x = 0;
+
 function setup() {
 	createCanvas(windowWidth, windowHeight);
+	background(100,66,100);
 }
 
 function draw() {
-	
+	fill(0,0,255);
+	ellipse(x,windowHeight/2,50,50);
+	x = x + 4;
 }
