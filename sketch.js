f0c2afeead3348f4d6aa4be888e872d63d4a2ed6
Philipp Korn <philipp.korn@ges.thm.de>
2018-10-08 10:52:43 +0200

    changed background behaviour and ellipse is not moving anymore

--- sketch.js
+++ sketch.js
@@ -1,12 +1,11 @@
-let x = 0;
+
 
 function setup() {
 	createCanvas(windowWidth, windowHeight);
-	background(100,66,100);
 }
 
 function draw() {
+	background(100,66,100);
 	fill(0,0,255);
-	ellipse(x,windowHeight/2,50,50);
-	x = x + 4;
+	ellipse(windowWidth/2,windowHeight/2,50,50);
 }
