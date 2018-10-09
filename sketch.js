ee9aaec8a86a66b2e0a81f583ddf4a6ddd99f6e2
Philipp Korn <philipp.korn@ges.thm.de>
2018-10-09 17:35:57 +0200

    added a rectangle

--- sketch.js
+++ sketch.js
@@ -8,4 +8,6 @@ function draw() {
 	background(100,66,100);
 	fill(0,0,255);
 	ellipse(windowWidth/2,windowHeight/2,50,50);
+	fill(255,255,0);
+	rect(windowWidth / 2 + 4, windowHeight / 2 * 4, 60,60);
 }
